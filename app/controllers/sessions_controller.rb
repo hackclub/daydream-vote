8b2d9e1e5e360e7fe6814a1f99eac616522767b2
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-09-27 13:05:03 +0100

    Add active storage, add nav bar, and add cards to a few pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,17 @@
 class SessionsController < ApplicationController
-
-
   def create
     email = params[:email]&.downcase&.strip
-    
+
     if email.blank?
       flash[:alert] = "Please enter an email address"
       render :new and return
     end
-    
+
     user = User.find_or_create_by(email: email)
     token = user.tokens.create!(expires_at: 3.hours.from_now)
-    
+
     LoopsMailer.sign_in_email(user.email, token.id).deliver_later
-    
+
     flash[:notice] = if Rails.env.development?
       "We sent you a login link. #{view_context.link_to('Check Letter Opener', letter_opener_web_path, target: '_blank')}".html_safe
     else
@@ -27,14 +25,14 @@ class SessionsController < ApplicationController
     flash[:notice] = "Successfully signed out"
     redirect_to root_path
   end
-  
+
   def verify_token
     token = Token.find_by(id: params[:token_id])
-    
+
     if token&.valid_for_login?
       token.mark_as_used!
       session[:user_id] = token.user.id
-      
+
       # Check if profile data exists
       if token.user.profile_datum.nil?
         flash[:notice] = "Successfully signed in! Please complete your profile information."
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def landing
+    if signed_in?
+      @projects = current_user.projects
+    end
   end
 end
