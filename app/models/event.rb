1a0f1db1b1f01e742b36255148bbfcbb59af1c5e
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-09-27 20:45:39 +0100

    Remove whitespace

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :projects, foreign_key: :attending_event_id, dependent: :restrict_with_error
   has_many :organizer_positions, dependent: :destroy
   has_many :organizers, through: :organizer_positions, source: :user
-  
+
   def to_param
     name
   end
