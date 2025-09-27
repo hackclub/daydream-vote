7c47e5a57e6c36928b81eded505bf08f1694bf9f
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-09-27 17:52:20 +0100

    Improve voting

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  
+
   has_many :projects, foreign_key: :attending_event_id, dependent: :restrict_with_error
 end
